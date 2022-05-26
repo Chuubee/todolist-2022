757bf10e3c27f45a18280959f62e34232fcb5539
Chuubee <ryu0612-ryuz@docomo.ne.jp>
2022-05-26 13:00:09 +0900

    second commit

---
+++ src/components/TodoList.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { ref } from "vue";
+
+const tasks = ref([
+    { name: "線形代数課題", done: false },
+    { name: "物理学実験レポート",done: false },
+])
+
+const newTaskName = ref("");
+const addTask = () => {
+    tasks.value.push({ name: newTaskName.value, done: false });
+};
+</script>
+
+<template>
+    <div>
+        <div>TodoList</div>
+        <div v-for="task in tasks" :key="task.name">
+            <div v-if="task.done == false">
+                <div class="task">
+                    <div class="name">
+                        未完: {{ task.name }}
+                            <button @click="task.done = true">完了</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div>
+            <div>Completed Tasks</div>
+            <div v-for="task in tasks" :key="task.name">
+                <div v-if="task.done == true">
+                    <div class="task">
+                        <div class="name">
+                            完了: {{ task.name }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <label>
+                タスクを入力: 
+                <input v-model="newTaskName" type="text" />
+            </label>
+            <button @click="addTask">add</button>
+        </div>
+    </div>
+</template>
+
+<style></style>
